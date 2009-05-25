aca19daa9e0347fc0ee6aee835ae3f2bf226d79e
Pierre Larochelle <pierre.larochelle@gatech.edu>
2009-05-24 17:44:40 -0700

    Now works with :use. Switched all standard libs to :require

--- test/poems.clj
+++ test/poems.clj
@@ -1,11 +1,11 @@
 (ns DailyPoem.test.poems
-  (:require (app.poems))
-  (:use (clojure xml) 
-	(clojure.contrib.test-is)))
+  (:require (clojure xml)
+	    (clojure.contrib test-is))
+  (:use (app.poems)))
 
 (def test-html
      "<html><body><a title=\"poem\" href=\"/wiki/This_Is_A_Poem\">This is a Poem</a></body></html>")
 
 
 (deftest test-get-elements-by-tag
-  (is (not-empty (poems/get-elements-by-tag html-node :a))))
+  (is (not-empty (DailyPoem.app.poems/get-elements-by-tag (clojure.xml/parse test-html) :a))))
