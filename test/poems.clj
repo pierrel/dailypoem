13f7ea6ba8314d15c9c877cedab6f09148617f43
Pierre Larochelle <pierre.larochelle@gatech.edu>
2009-05-24 17:32:59 -0700

    Using app.poems with ":require" works within the test poems file

--- test/poems.clj
+++ test/poems.clj
@@ -1,11 +1,11 @@
 (ns DailyPoem.test.poems
-  (:use app.poems)
-  (:use fact)
-  (:use clojure xml))
+  (:require (app.poems))
+  (:use (clojure xml) 
+	(clojure.contrib.test-is)))
 
 (def test-html
      "<html><body><a title=\"poem\" href=\"/wiki/This_Is_A_Poem\">This is a Poem</a></body></html>")
 
-(fact "The test-html variable has an \"a\" tag"
-      [html-node '((xml.parse test-html))]
-      (not-empty (get-elements-by-tag html-node :a)))
+
+(deftest test-get-elements-by-tag
+  (is (not-empty (poems/get-elements-by-tag html-node :a))))
